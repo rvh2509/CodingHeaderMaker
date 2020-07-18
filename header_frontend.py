c35d8a82da1fd21f03c370123746fbb7e921cfd7
Richard Halbert <rvh2509@outlook.com>
2020-07-17 20:13:11 -0700

    Made basic asking function in tkinter

--- header_frontend.py
+++ header_frontend.py
@@ -6,20 +6,29 @@ import tkinter
 window = tkinter.Tk()
 window.wm_title("Coding Header Maker")
 
-main_frame = tkinter.Frame(window)
-
-def clear_screen(frame):
-    widgets = frame.grid_slaves()
-    for widget in widgets:
-        widget.destroy()
+def clear_screen(win):
+    widgets = win.grid_slaves()
+    for element in widgets:
+        element.destroy()
 
 def ask_name():
+    clear_screen(window)
+    name_label = tkinter.Label(window, text="What's your name?")
+    name_label.grid(row=0, column=0)
+
+    name_text = tkinter.StringVar()
+    name_entry = tkinter.Entry(window, textvariable=name_text)
+    name_entry.grid(row=1, column=0)
+
+    sub = tkinter.Button(window, text="Submit", command=ask_date)
+    sub.grid(row=2, column=0)
 
 def ask_date():
+    clear_screen(window)
+#def ask_lang():
 
-def ask_lang():
+#def ask_class():
 
-def ask_class():
+#def ask_extra():
 
-def ask_extra():
 window.mainloop()
